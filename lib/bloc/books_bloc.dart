d37add14cf8c62e5ef076402b8daaafe902db4dc
pedro.garcia <pedro.garcia@toshibagcs.com>
2022-10-08 01:12:19 -0500

    Moved requests to repository file

--- lib/bloc/books_bloc.dart
+++ lib/bloc/books_bloc.dart
@@ -1,10 +1,9 @@
 import 'dart:async';
-import 'dart:convert';
 
 import 'package:bloc/bloc.dart';
+import 'package:bookshop/repositories/books_repository.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
-import 'package:http/http.dart' as http;
 
 part 'books_event.dart';
 part 'books_state.dart';
@@ -16,26 +15,23 @@ class BooksBloc extends Bloc<BooksEvent, BooksState> {
 
   Future<FutureOr<void>> _searchBooks(
       SearchBooksEvent event, Emitter emit) async {
+    dynamic booksList;
+
     // When clicking search emit LoadingState() which shows shimmer
     emit(LoadingState());
 
-    // Do http request
-    Uri url = Uri.parse(
-        "https://www.googleapis.com/books/v1/volumes?q=${event.bookToSearch}");
-
     try {
-      var response = await http.get(url);
-      var body = jsonDecode(response.body);
+      booksList = await BooksRepository().getBooks(event.bookToSearch);
 
-      if (body["totalItems"] == 0) {
+      if (booksList["totalItems"] == 0) {
         // If response has no books, emit NoResultsState
         emit(NoResultsState());
       } else {
         // If response has results, emit ShowResultsState (send books array)
-        emit(ShowResultsState(books: body["items"]));
+        emit(ShowResultsState(books: booksList["items"]));
       }
     } catch (e) {
-      // If request or response reading fails, emit ErrorState
+      // If request or response fails, emit ErrorState
       emit(ErrorState());
     }
   }
